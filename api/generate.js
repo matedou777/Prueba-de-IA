d60be4e052416c73c8e1df490d95221332407a39
Mateo <119713252+matedou777@users.noreply.github.com>
2026-04-16 13:58:37 -0300

    Change export syntax to CommonJS module format

--- api/generate.js
+++ api/generate.js
@@ -2,7 +2,7 @@
 // POST /api/generate
 // Body: { prompt: string, count: number }
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
